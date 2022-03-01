8d6385bb179875adba8fa749cd858a8b2388a8b2
rithwik-chithreddy-2000 <41821259+rithwik-chithreddy-2000@users.noreply.github.com>
2022-03-01 23:44:07 +0530

    Initial commit

--- frontend/src/app/core/components/layouts/Landing.js
+++ frontend/src/app/core/components/layouts/Landing.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div className="landing">
-      <div className="dark-overlay landing-inner text-light">
+    <div>
+      <div>
         <div className="container-fluid m-0">
           <div className="row">
             <div className="col-md-12 text-center">
               <h1 className="mb-4 text">Food Delivery</h1>
               <p className="lead">
-                Create a developer profile/portfolio, share posts and get help
-                from other developers
+                Welcome to my food app. Like Share and Subscribe. Have a great
+                time.
               </p>
               <hr />
 
